f032d49f409c15d0240a7edbeb4a576233f0692c
AlexMartin23 <832007@mystma.org>
2021-01-07 15:56:07 +0000

    comparisons try 1

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+
+def greaterOrEqual(x, y):
+    if x >= y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
+
+
+print(greaterThan(2, 1))
+
+print(lessThan(1, 2))
+
+print(equalTo(1, 1))
+
+print(greaterOrEqual(1, 1))
+
+print(lessOrEqual(1, 1))
